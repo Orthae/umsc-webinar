4e6d5181ddb41979eadb901b3b4604569b76c737
Orthae <orthaefluorite@gmail.com>
2021-05-09 08:29:57 +0200

    fix

--- src/main/java/orthae/com/github/umcs/PeselNotUniqueException.java
+++ src/main/java/orthae/com/github/umcs/PeselNotUniqueException.java
@@ -3,6 +3,6 @@ package orthae.com.github.umcs;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Pesel is not unique")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Pesel is not unique.")
 public class PeselNotUniqueException extends RuntimeException {
 }
